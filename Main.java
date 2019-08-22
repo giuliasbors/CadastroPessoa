3a34ba9e61bb5919afa406a174606f7380d7c744
giulia sbors <giulia_pereira@estudante.sc.senai.br>
2019-08-22 11:02:11 -0300

    finalizei projeto

---
+++ Main.java
@@ -0,0 +1,21 @@
+package cadastroPessoa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Endereco end1 = new Endereco (1,"rua das palmeiras", "fazenda", "sao jose");
+		Endereco end2 = new Endereco (2,"rua das cruzeiros", "pedra branca", "palhoca");
+		Endereco end3 = new Endereco (3,"rua dos juazeiros", "pedra branca", "palhoca");
+		
+		
+		Pessoa pes1 = new Pessoa (2, "22", "luana", "FEMININO");
+		Pessoa pes2 = new Pessoa (3, "30", "lucas", "MASCULINO");
+		Pessoa pes3 = new Pessoa (4, "55", "gabriel", "masculinO");
+		
+	}
+
+}
+ 
